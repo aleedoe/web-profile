eeeea59a3fa68053661d3ad58d2dfc703c113578
alie <mushyafaali@gmail.com>
2024-07-10 11:30:51 +0700

    update projects section

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,64 +1,29 @@
-import React from 'react'
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from './ui/card'
-import { Button } from './ui/button'
-import Image from 'next/image'
+"use client";
 
+import React from 'react';
+import Image from 'next/image';
+
+import { Button } from './ui/button';
+import { Card, CardDescription, CardFooter, CardHeader, CardTitle } from './ui/card';
+
+import { motion, Variants } from "framer-motion";
 import { HiDotsHorizontal } from "react-icons/hi";
 
-interface Project {
-    id: number;
-    title: string;
-    des: string;
-    img: string;
-    iconLists: string[];
-    link: string;
-}
+import { websiteProfile } from './ProjectContents';
 
-const contentWebsiteProfile: Project[] = [
-    {
-        id: 1,
-        title: "AI Image SaaS - Canva Application (2022) amd Tailwind CSS and React.js",
-        des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
-        img: "/project-1.png",
-        iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
-        link: "/ui.aiimg.com",
-    }, {
-        id: 1,
-        title: "AI Image SaaS - Canva Application (2022) amd Tailwind CSS and React.js",
-        des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
-        img: "/project-1.png",
-        iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
-        link: "/ui.aiimg.com",
-    }, {
-        id: 1,
-        title: "AI Image SaaS - Canva Application (2022) amd Tailwind CSS and React.js",
-        des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
-        img: "/project-1.png",
-        iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
-        link: "/ui.aiimg.com",
-    }, {
-        id: 1,
-        title: "AI Image SaaS - Canva Application (2022) amd Tailwind CSS and React.js",
-        des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
-        img: "/project-1.png",
-        iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
-        link: "/ui.aiimg.com",
-    }, {
-        id: 1,
-        title: "AI Image SaaS - Canva Application (2022) amd Tailwind CSS and React.js",
-        des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
-        img: "/project-1.png",
-        iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg"],
-        link: "/ui.aiimg.com",
-    }, {
-        id: 1,
-        title: "AI Image SaaS - Canva Application (2022) amd Tailwind CSS and React.js",
-        des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
-        img: "/project-1.png",
-        iconLists: ["/re.svg", "/tail.svg", "/ts.svg"],
-        link: "/ui.aiimg.com",
+const cardVariants: Variants = {
+    offscreen: {
+        y: 200
     },
-];
+    onscreen: (index: number) => ({
+        y: 0,
+        transition: {
+            type: "spring",
+            duration: 2,
+            delay: index * 0.1
+        }
+    })
+};
 
 const Project = () => {
     return (
@@ -67,35 +32,46 @@ const Project = () => {
             <div className='flex flex-col'>
                 <div className='flex flex-col'>
                     <h2 className='mb-6 text-2xl font-semibold'>Website Profiles</h2>
-                    <div className='flex flex-row flex-wrap items-center justify-center gap-4'>
-                        {contentWebsiteProfile.map((project, index) => (
-                            <Card className="w-[320px]" key={index}>
-                                <Image className='h-48 object-cover rounded-t-lg' alt='' src={project.img} width={350} height={0} />
-                                <CardHeader>
-                                    <CardTitle className='text-xl custom-card-title'>{project.title}</CardTitle>
-                                    <CardDescription className='custom-card-description'>{project.des}</CardDescription>
-                                </CardHeader>
-                                <CardFooter className="flex justify-between">
-                                    <div className="flex items-center">
-                                        {project.iconLists.slice(0, 4).map((icon, index) => (
-                                            <div
-                                                key={index}
-                                                className="border border-slate-100 rounded-full bg-white lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center"
-                                                style={{
-                                                    transform: `translateX(-${5 * index + 2}px)`,
-                                                }}
-                                            >
-                                                {index === 3 && project.iconLists.length > 4 ? (
-                                                    <HiDotsHorizontal />
-                                                ) : (
-                                                    <Image src={icon} alt="icon5" className="p-2" width={50} height={0}/>
-                                                )}
+                    <div className='flex flex-row flex-wrap items-center justify-center md:justify-around lg:justify-center gap-4'>
+                        {websiteProfile.map((project, index) => (
+                            <motion.div
+                                className="card-container"
+                                initial="offscreen"
+                                whileInView="onscreen"
+                                viewport={{ once: true }}
+                                custom={index}
+                                key={index}
+                            >
+                                <motion.div variants={cardVariants} custom={index}>
+                                    <Card className="w-[320px]">
+                                        <Image className='h-48 object-cover rounded-t-lg' alt='' src={project.img} width={350} height={0} />
+                                        <CardHeader>
+                                            <CardTitle className='text-xl custom-card-title'>{project.title}</CardTitle>
+                                            <CardDescription className='custom-card-description'>{project.des}</CardDescription>
+                                        </CardHeader>
+                                        <CardFooter className="flex justify-between">
+                                            <div className="flex items-center">
+                                                {project.iconLists.slice(0, 4).map((icon, index) => (
+                                                    <div
+                                                        key={index}
+                                                        className="border border-slate-100 rounded-full bg-white lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center"
+                                                        style={{
+                                                            transform: `translateX(-${5 * index + 2}px)`,
+                                                        }}
+                                                    >
+                                                        {index === 3 && project.iconLists.length > 4 ? (
+                                                            <HiDotsHorizontal />
+                                                        ) : (
+                                                            <Image src={icon} alt="icon5" className="p-2" width={50} height={0} />
+                                                        )}
+                                                    </div>
+                                                ))}
                                             </div>
-                                        ))}
-                                    </div>
-                                    <Button variant={"outline"}>Live Demo</Button>
-                                </CardFooter>
-                            </Card>
+                                            <Button variant={"outline"}>Live Demo</Button>
+                                        </CardFooter>
+                                    </Card>
+                                </motion.div>
+                            </motion.div>
                         ))}
                     </div>
                 </div>
@@ -113,4 +89,4 @@ const Project = () => {
     )
 }
 
-export default Project
+export default Project;
