8207a89955751961111ecc4c0f16722115cf9280
alie <mushyafaali@gmail.com>
2024-07-10 10:24:22 +0700

    update projects section

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -3,6 +3,8 @@ import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle }
 import { Button } from './ui/button'
 import Image from 'next/image'
 
+import { HiDotsHorizontal } from "react-icons/hi";
+
 interface Project {
     id: number;
     title: string;
@@ -12,48 +14,48 @@ interface Project {
     link: string;
 }
 
-const contentProjects: Project[] = [
+const contentWebsiteProfile: Project[] = [
     {
         id: 1,
-        title: "AI Image SaaS - Canva Application (2022) amd Tailwind CSS and React.js", 
+        title: "AI Image SaaS - Canva Application (2022) amd Tailwind CSS and React.js",
         des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
         img: "/project-1.png",
         iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
         link: "/ui.aiimg.com",
-    },{
+    }, {
         id: 1,
-        title: "AI Image SaaS - Canva Application (2022) amd Tailwind CSS and React.js", 
+        title: "AI Image SaaS - Canva Application (2022) amd Tailwind CSS and React.js",
         des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
         img: "/project-1.png",
         iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
         link: "/ui.aiimg.com",
-    },{
+    }, {
         id: 1,
-        title: "AI Image SaaS - Canva Application (2022) amd Tailwind CSS and React.js", 
+        title: "AI Image SaaS - Canva Application (2022) amd Tailwind CSS and React.js",
         des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
         img: "/project-1.png",
         iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
         link: "/ui.aiimg.com",
-    },{
+    }, {
         id: 1,
-        title: "AI Image SaaS - Canva Application (2022) amd Tailwind CSS and React.js", 
+        title: "AI Image SaaS - Canva Application (2022) amd Tailwind CSS and React.js",
         des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
         img: "/project-1.png",
         iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
         link: "/ui.aiimg.com",
-    },{
+    }, {
         id: 1,
-        title: "AI Image SaaS - Canva Application (2022) amd Tailwind CSS and React.js", 
+        title: "AI Image SaaS - Canva Application (2022) amd Tailwind CSS and React.js",
         des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
         img: "/project-1.png",
-        iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+        iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg"],
         link: "/ui.aiimg.com",
-    },{
+    }, {
         id: 1,
-        title: "AI Image SaaS - Canva Application (2022) amd Tailwind CSS and React.js", 
+        title: "AI Image SaaS - Canva Application (2022) amd Tailwind CSS and React.js",
         des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
         img: "/project-1.png",
-        iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+        iconLists: ["/re.svg", "/tail.svg", "/ts.svg"],
         link: "/ui.aiimg.com",
     },
 ];
@@ -66,16 +68,16 @@ const Project = () => {
                 <div className='flex flex-col'>
                     <h2 className='mb-6 text-2xl font-semibold'>Website Profiles</h2>
                     <div className='flex flex-row flex-wrap items-center justify-center gap-4'>
-                        {contentProjects.map((project, index) => (
+                        {contentWebsiteProfile.map((project, index) => (
                             <Card className="w-[320px]" key={index}>
-                                <Image className='h-48 object-cover rounded-t-lg' alt='' src={project.img} width={350} height={100} />
+                                <Image className='h-48 object-cover rounded-t-lg' alt='' src={project.img} width={350} height={0} />
                                 <CardHeader>
                                     <CardTitle className='text-xl custom-card-title'>{project.title}</CardTitle>
-                                    <CardDescription>{project.des}</CardDescription>
+                                    <CardDescription className='custom-card-description'>{project.des}</CardDescription>
                                 </CardHeader>
                                 <CardFooter className="flex justify-between">
                                     <div className="flex items-center">
-                                        {project.iconLists.map((icon, index) => (
+                                        {project.iconLists.slice(0, 4).map((icon, index) => (
                                             <div
                                                 key={index}
                                                 className="border border-slate-100 rounded-full bg-white lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center"
@@ -83,11 +85,15 @@ const Project = () => {
                                                     transform: `translateX(-${5 * index + 2}px)`,
                                                 }}
                                             >
-                                                <img src={icon} alt="icon5" className="p-2" />
+                                                {index === 3 && project.iconLists.length > 4 ? (
+                                                    <HiDotsHorizontal />
+                                                ) : (
+                                                    <Image src={icon} alt="icon5" className="p-2" width={50} height={0}/>
+                                                )}
                                             </div>
                                         ))}
                                     </div>
-                                    <Button>Deploy</Button>
+                                    <Button variant={"outline"}>Live Demo</Button>
                                 </CardFooter>
                             </Card>
                         ))}
