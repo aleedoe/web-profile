ca7dbfa3e8718f547fb9eb6b3c8d92bd85495b2d
alie <mushyafaali@gmail.com>
2024-07-12 11:13:26 +0700

    update footer section

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,6 +4,7 @@ import { FaFacebook } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { RiInstagramFill } from "react-icons/ri";
 import { FaLinkedin } from "react-icons/fa";
+import Link from 'next/link';
 
 
 const Footer = () => {
@@ -11,10 +12,18 @@ const Footer = () => {
         <section className='py-12 flex flex-col md:flex-row lg:flex-row justify-between items-center px-8 flex-wrap gap-3'>
             <h1 className='font-bold'>© 2023 Webzet.dev All Rights Reserved.</h1>
             <div className='flex gap-4 flex-row'>
-                <FaFacebook size={30}/>
-                <FaGithub size={30}/>
-                <RiInstagramFill size={30}/>
-                <FaLinkedin size={30}/>
+                <Link href="https://www.facebook.com/mushyafa.ali.12/" target="_blank">
+                    <FaFacebook size={30} />
+                </Link>
+                <Link href="https://www.facebook.com/mushyafa.ali.12/" target="_blank">
+                    <FaLinkedin size={30} />
+                </Link>
+                <Link href="https://www.facebook.com/mushyafa.ali.12/" target="_blank">
+                    <RiInstagramFill size={30} />
+                </Link>
+                <Link href="https://www.facebook.com/mushyafa.ali.12/" target="_blank">
+                    <FaGithub size={30} />
+                </Link>
             </div>
         </section>
     )
